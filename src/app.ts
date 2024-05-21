9a2b1682f7c78e78a05f9c44d6d4e75f1ad35277
123Somrat <jafaruddin875@gmail.com>
2024-05-21 11:37:34 +0200

    Create different folder for all middleware

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,9 @@
 import express, { Request, Response } from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv'
-dotenv.config()
-
+import applyMiddleware from './middleware';
 
 // EXpress app
 const app = express();
-
-
-// Middleware for cors and pasrse req body
-app.use(express.json());
-app.use(cors());
-
-
+applyMiddleware(app);
 
 // Appliication Health cheakUp Route
 app.get('/health', (req: Request, res: Response) => {
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import http from 'http';
 import app from './app';
-
+import dotenv from 'dotenv';
+dotenv.config();
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 
