6944d1428cc9f4cbab6de1c70b7b4ec07c6c9f7e
123Somrat <jafaruddin875@gmail.com>
2024-05-21 14:35:19 +0200

    Create a example route for testing

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,9 +1,11 @@
 import express, { Application } from 'express';
 import cors from 'cors';
+import router from '../router';
 
 const applyMiddleware = (app: Application) => {
   app.use(express.json());
   app.use(cors());
+  app.use(router)
 };
 
 export default applyMiddleware;
