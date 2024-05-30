1907adae933d4112dfbd0e2315d7c1a005023956
123Somrat <jafaruddin875@gmail.com>
2024-05-30 11:46:26 +0200

    Create orderType and orderSchema and model

---
+++ src/model/order/order.ts
@@ -0,0 +1,25 @@
+import { Schema, model } from 'mongoose';
+import orderType from './orderType';
+
+const order = new Schema<orderType>({
+  email: {
+    type: String,
+    required: true,
+  },
+  productId: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Order = model('Order', order);
+
+export default Order;
---
+++ src/model/order/orderType.ts
@@ -0,0 +1,8 @@
+interface orderType {
+  email: string;
+  productId: string;
+  price: number;
+  quantity: number;
+}
+
+export default orderType;
